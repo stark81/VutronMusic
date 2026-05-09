0005830fcba554fa355d9a3447232c291fc1a55e
ling-yumeng <ff-c109@outlook.com>
2026-05-10 01:10:12 +0800

    flatpak don't need sandbox, so remove fix-sandbox.js (require root, bad code)

--- fix-sandbox.js
+++ fix-sandbox.js
@@ -4,6 +4,6 @@ if (process.platform === 'linux') {
   // You need to make sure that
   // /home/runner/work/VutronMusic/VutronMusic/node_modules/electron/dist/chrome-sandbox
   // is owned by root and has mode 4755.
-  execSync('sudo chown root:root ./node_modules/electron/dist/chrome-sandbox')
-  execSync('sudo chmod 4755 ./node_modules/electron/dist/chrome-sandbox')
+  //execSync('sudo chown root:root ./node_modules/electron/dist/chrome-sandbox')
+  //execSync('sudo chmod 4755 ./node_modules/electron/dist/chrome-sandbox')
 }
